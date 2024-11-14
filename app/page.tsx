1b3e54689421171910beb2a780971672dfc5b1d9
Kolia47 <159222847+Kolia47@users.noreply.github.com>
2024-11-14 22:41:01 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="bg-blue-300 min-h-screen p-2">
+      <h1>Welcome to my Website</h1>
+      <p>
+        Welcome to my website its my first project and will be shown in github pages
+      </p>
+    </div>
+  )
+}
